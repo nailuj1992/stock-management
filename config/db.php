373ef2d3fe66dfa9a3036b733705382a4156aa65
Julian Gonzalez Prieto <juliangonzalezprieto@hotmail.com>
2025-02-21 21:41:32 -0500

    db credentials on dev

--- config/db.php
+++ config/db.php
@@ -1,14 +1,21 @@
 <?php
+$YII_ENV_DEV = YII_ENV === 'dev';
+if ($YII_ENV_DEV) {
+    $host = '127.0.0.1';
+    $db = 'stock_management';
+    $user = 'root';
+    $pwd = '';
+}
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=stock_management',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=' . $host . ';dbname=' . $db,
+    'username' => $user,
+    'password' => $pwd,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => $YII_ENV_DEV,
+    'schemaCacheDuration' => $YII_ENV_DEV ? 60 : null,
+    'schemaCache' => $YII_ENV_DEV ? 'cache' : null,
 ];
